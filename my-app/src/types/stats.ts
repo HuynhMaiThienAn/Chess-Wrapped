b9303ac4554fd7f6407d0f69d66f66b57a95561c
HuynhMaiThienAn <s3998345@rmit.edu.vn>
2025-12-10 10:40:08 +0700

    feat: define user game stats for a year
    showcase your chess journey (no brilliant and blunder showcase :)

---
+++ my-app/src/types/stats.ts
@@ -0,0 +1,51 @@
+export interface GameStats {
+    year: number;
+
+    // General Activity
+    totalGames: number;
+    totalHours: number;
+    gamesByVariant: { name: string; count: number }[];
+    mostPlayedVariant: string;
+
+    // Elo
+    eloHistory: { date: string; rating: number; monthIndex: number }[];
+    eloChange: number;
+
+    // Openings
+    uniqueWhiteVariants: number;
+    uniqueBlackVariants: number;
+    topOpeningsWhite: OpeningStat[];
+    topOpeningsBlack: OpeningStat[];
+
+    // worse openings to play against
+    worstOpeningsWhite: OpeningStat[];
+    worstOpeningsBlack: OpeningStat[];
+
+    // Highlights
+    impressiveMatches: {
+        opponent: string;
+        opponentElo: number;
+        eloGap: number;
+        date: string;
+        url: string;
+        timeControl: string;
+        opponentAvatarUrl: string;
+    }[];
+
+    tournamentCount: number;
+    tournamentSummary?: TournamentStats[];
+}
+
+export interface OpeningStat {
+    name: string;
+    count: number;
+    winRate: number;
+    highestWinElo: number;
+    pgn?: string;
+}
+
+export interface TournamentStats {
+    variant: string;
+    count: number;
+    winRate: number;
+}
