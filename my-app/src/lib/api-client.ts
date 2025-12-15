b5bb7ab2556c79c14c6a34ec5852b6fa74bd0a4d
HuynhMaiThienAn <s3998345@rmit.edu.vn>
2025-12-15 10:27:51 +0700

    revert: redis is too overkill for now

--- my-app/src/lib/api-client.ts
+++ my-app/src/lib/api-client.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const BASE_URL = process.env.NEXT_PUBLIC_CHESS_API_URL || "https://api.chess.com/pub";
 
 export const apiRequest = async <T>(
@@ -21,4 +22,30 @@ export const apiRequest = async <T>(
     }
 
     return res.json();
+=======
+import axios, {AxiosResponse} from "axios";
+/*
+Reference from
+https://medium.com/@ignatovich.dm/creating-a-type-safe-api-client-with-typescript-and-react-ce1b82bf8b9b
+*/
+
+const apiClient = axios.create({
+    baseURL: "https://api.chess.com/pub",
+    headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': '/1.0 (contact: huynhmaithienan.2005@gmail.com)'
+    },
+    timeout: 10000 // wait for 10s cuz why not
+})
+
+// generic API function
+export const apiRequest = async <T>(url: string, method: 'GET' | 'POST', data?: any): Promise<T> => {
+    const response: AxiosResponse<T> = await apiClient({
+        method,
+        url,
+        data,
+    });
+
+    return response.data;
+>>>>>>> parent of 5c0ac45 (feat: use fetch with cache data to speed up and reduce api calls)
 };
