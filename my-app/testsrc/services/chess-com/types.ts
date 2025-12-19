8916b6d25d3ec43140173d6881fa84d3b5282a9d
HuynhMaiThienAn <s3998345@rmit.edu.vn>
2025-12-19 14:40:42 +0700

    feat: remove current structure (due to lack of understanding, need further investigation)

--- my-app/testsrc/services/chess-com/types.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import * as Schemas from "./schemas";
-
-export type RawProfileResponse = z.infer<typeof Schemas.RawProfileSchema>;
-export type RawArchivesResponse = z.infer<typeof Schemas.RawArchivesSchema>;
-export type RawMonthlyGamesResponse = z.infer<typeof Schemas.RawMonthlyGamesSchema>;
